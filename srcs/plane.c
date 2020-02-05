67aa5e74af0d77901736d2d3e2ced5e5ec1af694
Hamza Ezzahiry <hezzahir@e1r9p11.1337.ma>
2020-02-05 14:14:51 +0100

    fix plane

--- srcs/plane.c
+++ srcs/plane.c
@@ -6,7 +6,7 @@
 /*   By: hezzahir <hamza.ezzahiry@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/04 21:54:26 by hezzahir          #+#    #+#             */
-/*   Updated: 2020/02/02 12:14:10 by hezzahir         ###   ########.fr       */
+/*   Updated: 2020/02/04 20:51:01 by hezzahir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,6 @@
 /*
 **	Plane <origin> <color> <norme> <translation> <rotation>
 */
-
-void	plane_rot_tran(t_plane *plane, char *s1, char *s2)
-{
-	plane->tran = get_vect_from_str(s1);
-	plane->rot = get_vect_from_str(s2);
-	plane->origin = trans(plane->origin, plane->tran);
-	plane->norm = rotate(plane->norm, plane->rot);
-}
 
 void	set_plane(char **words, t_rtv1 *r)
 {
@@ -35,7 +27,6 @@ void	set_plane(char **words, t_rtv1 *r)
 	plane->origin = get_vect_from_str(words[1]);
 	plane->color = get_color_from_str(words[2]);
 	plane->norm = get_vect_from_str(words[3]);
-	plane_rot_tran(plane, words[4], words[5]);
 	shape->shape = plane;
 	shape->id = 2;
 	shape->next = NULL;
